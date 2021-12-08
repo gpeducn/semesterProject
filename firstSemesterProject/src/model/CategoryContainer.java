e7ff2d020651684d25587926fd7d53479bb09962
Gabriel Pędlowski <10407544@ucn.dk>
2021-12-09 00:05:22 +0100

    test tings

--- firstSemesterProject/src/model/CategoryContainer.java
+++ firstSemesterProject/src/model/CategoryContainer.java
@@ -1,5 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
 public class CategoryContainer {
 
+	private static CategoryContainer instance;
+	private ArrayList<Category> categories;
+	
+	private CategoryContainer() {
+		categories = new ArrayList<>();
+	}
+	
+	public static CategoryContainer getInstance() {
+		if(instance == null) {
+			instance = new CategoryContainer();
+		}
+		return instance;
+	}
+	
+	
 }
--- firstSemesterProject/src/ui/TryMe.java
+++ firstSemesterProject/src/ui/TryMe.java
@@ -1,5 +1,32 @@
 package ui;
 
+import model.*;
+
 public class TryMe {
 
+	private Category category1;
+	private Category category2;
+	
+	private Customer customer1;
+	private Customer customer2;
+	
+	private Employee employee1;
+	private Employee employee2;
+	
+	private Order order1;
+	private Order order2;
+	
+	private Product product1;
+	private Product product2;
+	
+	public TryMe() {
+		
+	}
+	
+	public void newData() {
+		category1 = new Category("Garden tools");
+		category2 = new Category("Furnitures");
+		
+		CategoryContainer.ge
+	}
 }
