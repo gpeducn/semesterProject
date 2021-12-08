74ad9090c23b5e24f68622e34d89ed4c27ce31e0
Boti28 <budai.boti03@gmail.com>
2021-12-08 16:49:06 +0100

    nice

--- firstSemesterProject/src/model/Customer.java
+++ firstSemesterProject/src/model/Customer.java
@@ -1,5 +1,45 @@
-package model;
+import java.util.Calendar;
 
-public class Customer {
 
+public abstract class Customer {
+	private Calendar registrationDate;
+	private String name;
+	private String phoneNumber;
+	private int zipCode;
+	private String address;
+	
+	public Customer(String name, String phoneNumber, int zipCode, String address) {
+		super();
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.zipCode = zipCode;
+		this.address = address;
+		registrationDate = Calendar.getInstance();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public int getZipCode() {
+		return zipCode;
+	}
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}	
 }
+
