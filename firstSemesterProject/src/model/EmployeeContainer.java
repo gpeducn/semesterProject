f4a49d787d5a75a5c999bde1b692aa80012c7124
BrynjarDadi <brynjardadi@icloud.com>
2021-12-09 11:09:43 +0100

    employee stuff

--- firstSemesterProject/src/model/EmployeeContainer.java
+++ firstSemesterProject/src/model/EmployeeContainer.java
@@ -1,6 +1,26 @@
 package model;
 
+import java.util.ArrayList;
+
 public class EmployeeContainer {
+	private Employee employee;;
+	private  CustomerContainer stEmployee;
+	private ArrayList<Employee> employees;
+	
+	public ArrayList getInstane() {
+		return employees;
+	}
+	
+	public boolean addEmployee(Employee employee) {
+		return employees.add(employee);
+	}
+	
+	public boolean removeEmployee(String name) {
+		return employees.remove(employee);
+	}
+	
+	//implement read, update, search
+
 	public void xd() {
 		System.out.println("xd");
 	}
